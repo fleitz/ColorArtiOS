1ec0426f3aca4b6d4bae64290661b3935fdffe48
Fred Leitz <fred.leitz@gmail.com>
2012-12-12 01:12:48 -0800

    initial port of Color Art to iOS

--- ColorArtiOS/CAAppDelegate.m
+++ ColorArtiOS/CAAppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "CAAppDelegate.h"
+#import "CAViewController.h"
 
 @implementation CAAppDelegate
 
@@ -15,6 +16,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    [self.window setRootViewController:[[CAViewController alloc] init]];
     [self.window makeKeyAndVisible];
     return YES;
 }
